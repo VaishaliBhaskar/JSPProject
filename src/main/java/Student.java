905b6b5e3f92638b60e77b59c6a83064d2a2da1b
Vaishali <vaishalibhaskar2015@gmail.com>
2023-09-04 20:40:38 +0530

    Static variable a is added

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,8 +1,10 @@
 public class Student{
- public static void main(String[] args){
+static int a=10;
+public static void main(String[] args){
 
 System.out.println("This is student java file");
 System.out.println("This is new line added to the Student file");
+Syste.out.println("value of static variable a= "+a);
 
 }
 
